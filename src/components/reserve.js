fe5c3ee7d910f8543ae4e1b2ce2c6dfb84747d49
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-05-29 15:05:38 +0800

    added placeholder for list of lab classrooms

--- src/components/reserve.js
+++ src/components/reserve.js
@@ -2,13 +2,25 @@ let loggedInUser = null;
 let accounts = [];
 let currentAccount = null;
 
+const loggedInUserElement = document.getElementById('logged--in--element');
+
 document.addEventListener('DOMContentLoaded', () => {
     getStorageData();
 
     currentAccount = accounts[loggedInUser];
 
-    document.title = `Dashboard - ${currentAccount.email}`;
+    document.title = `Reserve - ${currentAccount.email}`;
     loggedInUserElement.textContent = `${currentAccount.firstName} ${currentAccount.lastName}`;
+});
+
+loggedInUserElement.addEventListener('click', () => {
+    const userMenu = document.getElementById('user--dropdown');
+
+    if (userMenu.style.display === 'block') {
+        userMenu.style.display = 'none';
+    } else {
+        userMenu.style.display = 'block';
+    }
 });
 
 function getStorageData() {
