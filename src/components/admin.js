fcea593a04e6fdf3f08db494cff0b8d6b61bb6fa
userisdabeh <120602326+userisdabeh@users.noreply.github.com>
2025-06-08 15:13:46 +0800

    project reset

--- src/components/admin.js
+++
@@ -1,9 +0,0 @@
-let gokoLabRooms = [];
-
-function getStorageData() {
-    gokoLabRooms = JSON.parse(localStorage.getItem('gokoLabRooms')) || [];
-}
-
-function saveStorageData() {
-    localStorage.setItem('gokoLabRooms', JSON.stringify(gokoLabRooms));
-}
