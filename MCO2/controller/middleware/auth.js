349ba5120d0e38ccf9ae1d235ea9de28f1725382
MeerKT <166136002+MeerKT@users.noreply.github.com>
2025-07-13 23:40:12 +0800

    remember-me

--- MCO2/controller/middleware/auth.js
+++ MCO2/controller/middleware/auth.js
@@ -4,3 +4,24 @@ function isAuthenticated(req, res, next) {
 }
 
 module.exports = isAuthenticated;
+
+//remember-me
+module.exports = (req, res, next) => {
+    if (req.session.userId) return next();
+
+    if (req.cookies.remember) {
+        req.session.userId = req.cookies.remember;
+
+        // Refresh the cookie for another 3 weeks
+        res.cookie('remember', req.cookies.remember, {
+            maxAge: 1000 * 60 * 60 * 24 * 21, // reset 3 weeks
+            httpOnly: true
+        });
+
+        return next();
+    }
+
+    // No session and no cookie: force login
+    res.redirect('/');
+};
+
