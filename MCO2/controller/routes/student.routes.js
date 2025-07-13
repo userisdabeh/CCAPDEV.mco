ed0aad69911bb11a0b58ae324285978b916e56fc
cruz-jullian <138432216+cruz-jullian@users.noreply.github.com>
2025-07-13 18:25:22 +0800

    Update student.routes.js
    added edit

--- MCO2/controller/routes/student.routes.js
+++ MCO2/controller/routes/student.routes.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
 const User = require('../../model/user.model.js');
 const Room = require('../../model/room.model.js');
 const Reservation = require('../../model/reservation.model.js');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage });
 
 function getNext7DatesExcludingSundays() {
     const dates = [];
@@ -25,7 +38,6 @@ router.get('/student/dashboard/:id', async (req, res) => {
 
     try {
         const user = await User.findOne({ _id: studentID }).lean();
-
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
@@ -48,7 +60,6 @@ router.get('/student/reserve/:id', async (req, res) => {
 
     try {
         const user = await User.findOne({ _id: studentID }).lean();
-
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
@@ -77,7 +88,6 @@ router.get('/student/profile/:id', async (req, res) => {
 
     try {
         const user = await User.findOne({ _id: studentID }).lean();
-
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
         }
@@ -97,6 +107,31 @@ router.get('/student/profile/:id', async (req, res) => {
     } catch (error) {
         console.error('Error fetching user:', error);
         res.status(500).json({ error: 'Failed to fetch user' });
+    }
+});
+
+router.post('/student/update-profile/:id', upload.single('profilePicture'), async (req, res) => {
+    try {
+        const userID = req.params.id;
+        const { firstName, lastName, email, aboutMe } = req.body;
+
+        const updateData = {
+            firstName,
+            lastName,
+            email,
+            aboutMe
+        };
+
+        // prof pic upload
+        if (req.file) {
+            updateData.profilePicture = `/uploads/${req.file.filename}`;
+        }
+
+        await User.findByIdAndUpdate(userID, updateData, { new: true });
+        res.redirect(`/student/profile/${userID}`);
+    } catch (err) {
+        console.error('Profile update error:', err);
+        res.status(500).send('Failed to update profile');
     }
 });
 
